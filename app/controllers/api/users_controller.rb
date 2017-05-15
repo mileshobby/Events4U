f848ed994b05188395fad6563871c803ed89a21c
Miles Hobby <mhobby16@gmail.com>
2017-05-15 14:25:29 -0700

    add users controller with functional create action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,18 @@
 class Api::UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      log_in(@user)
+      render json: @user #maybe change this to just render a partial
+    else
+      render json: @user.errors.full_messages, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
