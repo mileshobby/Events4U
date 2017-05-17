452189e6620166c12b73c130e093edeb0e3c25b2
Miles Hobby <mhobby16@gmail.com>
2017-05-17 10:12:03 -0700

    finish events controller

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,4 +1,50 @@
 class Api::EventsController < ApplicationController
+
   def index
+    @events = Event.all
   end
+
+  def create
+    @event = Event.new(event_params)
+    @event.host = current_user
+    if @event.save
+      render :show
+    else
+      render json: @event.errors_full_messages, status: 400
+    end
+  end
+
+  def destroy
+    @event = Event.find_by(id: params[:id])
+    if @event
+      @event.destroy
+      render json: {}
+    else
+      render json: ["We could not find this event in our database."], status: 404
+    end
+  end
+
+  def update
+    @event = Event.find_by(id: params[:id])
+    if @event && @event.user == current_user
+      if @event.update_attributes(event_params)
+        render :show
+      else
+        render json: @event.errors.full_messages, status: 400
+      end
+    elsif @event #case that the user tried to update an event that was not theirs
+      render json: ["You can only update events that you are hosting!"], status: 400
+    else #case event was not found
+      render json: ["We could not find this event in our database."], status: 404
+    end
+  end
+
+  def event_params
+    params
+    .require(:events)
+    .permit(:title, :short_description, :full_description,
+            :image_url, :date, :total_quantity,
+            :quantity_left, :price)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :api, { format: :json } do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
+    resources :events, only: [:create, :destroy, :update, :index]
   end
 end
