860e92324736e3de2aade6f3e4f569e64d3a0bd5
Miles Hobby <mhobby16@gmail.com>
2017-05-16 19:13:41 -0700

    create home component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,15 +4,14 @@ import { Route } from 'react-router';
 import GreetingContainer from './auth/greeting_container';
 import { AuthRoute } from '../util/route_util';
 import NavBarContainer from './nav/nav_bar_container';
+import Home from './home/home';
 
 const App = () => (
   <div id='app'>
     <header>
       <NavBarContainer />
     </header>
-    <div id="main-img-holder">
-      <div id="main-img-container"></div>
-    </div>
+    <Route exact path="/" component={Home} />
   </div>
 );
 
---
+++ frontend/components/home/home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Home extends React.Component{
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div id="home">
+        <header id="main-img-holder">
+          <div id="main-img-container"></div>
+        </header>
+      </div>
+    );
+  }
+
+}
+
+export default Home;
