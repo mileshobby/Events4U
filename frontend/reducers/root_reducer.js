65dbbc364d0a8f00f1598075f7714e294087f0b9
Miles Hobby <mhobby16@gmail.com>
2017-05-17 11:44:15 -0700

    add eventDetails reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux';
 import UsersReducer from './users_reducer';
 import ErrorsReducer from './errors_reducer';
+import EventsReducer from './events_reducer';
+import EventDetailsReducer from './event_details_reducer';
 
 
 const rootReducer = combineReducers({
   currentUser: UsersReducer,
-  errors: ErrorsReducer
+  errors: ErrorsReducer,
+  events: EventsReducer,
+  eventDetails: EventDetailsReducer
 });
 
 export default rootReducer;
