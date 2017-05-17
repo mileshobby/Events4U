9a4689db00cf88a3a7e5b0ce08eae449a81ccf63
Miles Hobby <mhobby16@gmail.com>
2017-05-16 20:40:46 -0700

    prepare for initial Heroku push

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -9,9 +9,8 @@ class Home extends React.Component{
   render(){
     return(
       <div id="home">
-        <header id="main-img-holder">
-          <div id="main-img-container"></div>
-        </header>
+        <img src="/assets/color_splash.jpg" id="main-home-img"></img>
+        <SearchBox />
       </div>
     );
   }
--- frontend/components/modal/auth_modal.jsx
+++ frontend/components/modal/auth_modal.jsx
@@ -30,7 +30,7 @@ class AuthModal extends React.Component{
           left              : 0,
           right             : 0,
           bottom            : 0,
-          backgroundColor   : 'rgba(255, 255, 255, 0.5)'
+          backgroundColor   : 'rgba(76, 76, 76, 0.7)'
         },
         content : {
           top                   : '40%',
