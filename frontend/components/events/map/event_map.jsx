ef4e272b7ba258fe4ea86954e47c36c3c3fea957
Miles Hobby <mhobby16@gmail.com>
2017-05-21 20:48:27 -0700

    add icons that display event title on mouseover of event

--- frontend/components/events/map/event_map.jsx
+++ frontend/components/events/map/event_map.jsx
@@ -12,7 +12,7 @@ class EventMap extends React.Component{
       zoom: 10
     };
     this.map = new google.maps.Map(this.mapNode, mapOptions);
-    this.MarkerManager = new MarkerManager(this.map);
+    this.MarkerManager = new MarkerManager(this.map, this.handleMarkerClick.bind(this));
     this.MarkerManager.updateMarkers(this.props.events);
   }
 
@@ -24,6 +24,10 @@ class EventMap extends React.Component{
     } else {
       this.MarkerManager.updateMarkers(this.props.events);
     }
+  }
+
+  handleMarkerClick(event) {
+    this.props.history.push(`events/${event.id}`);
   }
 
   render(){
